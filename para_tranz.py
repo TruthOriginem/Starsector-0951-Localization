60a356c30d341135b5a26223d67fc20a59ea98be
jn_xyp <jinan.xyp@gmail.com>
2021-12-03 19:53:46 +0800

    补充注释

--- para_tranz.py
+++ para_tranz.py
@@ -9,17 +9,20 @@ from pathlib import Path
 
 from typing import Set, Dict, Tuple, List, Union
 
+# 设置日志输出
 logging.basicConfig()
 logging.root.setLevel(logging.NOTSET)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.NOTSET)
 
+# 设置游戏原文，译文和Paratranz数据文件路径
 PROJECT_DIRECTORY = Path(__file__).parent
 ORIGINAL_PATH = PROJECT_DIRECTORY / 'original'
 TRANSLATION_PATH = PROJECT_DIRECTORY / 'localization'
 PARA_TRANZ_PATH = PROJECT_DIRECTORY / 'para_tranz'
 
 
+# 尝试计算相对于根目录的位置
 def relative_path(path: Path) -> Path:
     try:
         return path.relative_to(PROJECT_DIRECTORY)
@@ -27,6 +30,7 @@ def relative_path(path: Path) -> Path:
         return path
 
 
+# 用于表示词条的数据结构，创建后为只读
 @dataclass(frozen=True)
 class String:
     key: str
@@ -41,6 +45,7 @@ class String:
         return d
 
 
+# 用于表示游戏原文、译文文件的抽象类
 class DataFile:
     def __init__(self, path: Path, original_path: Path = None, translation_path: Path = None):
         self.path = Path(path)  # 相对 original 或者 localization 文件夹的路径
@@ -56,25 +61,29 @@ class DataFile:
         pass
 
 
+# 用于表示csv格式游戏原文、译文文件的类
 class CsvFile(DataFile):
     def __init__(self, path: Path, id_column_name: str, text_column_names: Set[str],
                  original_path: Path = None, translation_path: Path = None):
         super().__init__(path, original_path, translation_path)
         # csv 中作为 id 的列名
-        self.id_column_name = id_column_name  # type:Union[str, List[str]]
-        self.text_column_names = text_column_names  # 包含要翻译文本的列名
+        self.id_column_name = id_column_name  # type:Union[str, List[str]] # 作为id的列名，可能为多个
+        self.text_column_names = text_column_names  # type:Set[str]  # 包含要翻译文本的列名
 
-        self.column_names = []
+        self.column_names = []  # type:List[str]
 
-        self.original_data = []
-        self.original_id_data = {}
-        self.translation_data = []
-        self.translation_id_data = {}
+        # 原文数据，以及id列内容到数据的映射
+        self.original_data = []  # type:List[Dict]
+        self.original_id_data = {}  # type:Dict[str, Dict]
+        # 译文数据，以及id列内容到数据的映射
+        self.translation_data = []  # type:List[Dict]
+        self.translation_id_data = {}  # type:Dict[str, Dict]
 
         self.load_original_and_translation_data()
 
         self.validate()
 
+    # 从原文和译文csv中读取数据
     def load_original_and_translation_data(self) -> None:
         self.column_names, self.original_data, self.original_id_data = self.load_csv(
             self.original_path,
@@ -88,6 +97,7 @@ class CsvFile(DataFile):
             logger.info(
                 f'从 {relative_path(self.translation_path)} 中加载了 {len(self.translation_data)} 行译文数据，其中未被注释且不为空的行数为 {len(self.translation_id_data)}')
 
+    # 读取完毕后检查数据有效性
     def validate(self):
         # 检查指定的id列和文字列在游戏文件中是否存在
         if (type(self.id_column_name) == str and self.id_column_name not in self.column_names) and (
@@ -105,6 +115,7 @@ class CsvFile(DataFile):
             logger.warning(
                 f'文件 {relative_path(self.path)} 所加载的未被注释且不为空的原文与译文在去数据量不匹配：加载有效原文 {len(self.original_id_data)} 条，有效译文 {len(self.translation_id_data)} 条')
 
+    # 将数据转换为 ParaTranz 词条数据对象
     def get_strings(self) -> Set[String]:
         strings = set()
         for row_id, row in self.original_id_data.items():
@@ -122,6 +133,21 @@ class CsvFile(DataFile):
                     strings.add(String(key, original, translation))
         return strings
 
+    # 将数据转换为 ParaTranz 词条数据对象，并保存到json文件中
+    def save_strings_json(self, ensure_ascii=False) -> None:
+        strings = [s for s in self.get_strings() if s.original]  # 只导出原文不为空的词条
+
+        self.para_tranz_path.parent.mkdir(parents=True, exist_ok=True)
+        with open(self.para_tranz_path, 'w') as f:
+            data = []
+            for string in strings:
+                data.append(string.as_dict())
+            json.dump(data, f, ensure_ascii=ensure_ascii)
+
+        logger.info(
+            f'从 {relative_path(self.path)} 中导出了 {len(strings)} 个词条到 {relative_path(self.para_tranz_path)}')
+
+    # 将传入的 ParaTranz 词条数据对象中的译文数据合并到现有数据中
     def update_strings(self, strings: Set[String]) -> None:
         for s in strings:
             _, id, column = re.split('[#$]', s.key)
@@ -129,6 +155,7 @@ class CsvFile(DataFile):
                 self.translation_id_data[id][
                     column] = s.translation if s.translation else s.original
 
+    # 从json文件读取 ParaTranz 词条数据对象中的译文数据合并到现有数据中
     def update_strings_from_json(self) -> None:
         if self.para_tranz_path.exists():
             strings = set()
@@ -145,19 +172,7 @@ class CsvFile(DataFile):
         else:
             logger.warning(f'未找到 {self.path} 所对应的 ParaTranz 数据 ({self.para_tranz_path})，未更新词条')
 
-    def save_strings_json(self, ensure_ascii=False) -> None:
-        strings = [s for s in self.get_strings() if s.original]  # 只导出原文不为空的词条
-
-        self.para_tranz_path.parent.mkdir(parents=True, exist_ok=True)
-        with open(self.para_tranz_path, 'w') as f:
-            data = []
-            for string in strings:
-                data.append(string.as_dict())
-            json.dump(data, f, ensure_ascii=ensure_ascii)
-
-        logger.info(
-            f'从 {relative_path(self.path)} 中导出了 {len(strings)} 个词条到 {relative_path(self.para_tranz_path)}')
-
+    # 将译文数据写回译文csv中
     def save_translation_data(self) -> None:
         with open(self.translation_path, 'r', newline='') as f:
             csv = reader(f)
@@ -180,6 +195,12 @@ class CsvFile(DataFile):
     @staticmethod
     def load_csv(path: Path, id_column_name: Union[str, List[str]]) -> Tuple[
         List[str], List[Dict], Dict[str, Dict]]:
+        """
+        从csv中读取数据，并返回列名列表，数据以及id列内容到数据的映射
+        :param path: csv文件路径
+        :param id_column_name: id列名称，只有一列的话传入列名，有多列传入列名list
+        :return: (列名列表, 数据list, id列内容到数据的映射dict)
+        """
         data = []
         id_data = {}
         with open(path, 'r', errors="surrogateescape") as csv_file:
@@ -202,14 +223,38 @@ class CsvFile(DataFile):
 
 
 # https://segmentfault.com/a/1190000017940752
+# 判断是否包含中文字符
 def contains_chinese(s: str) -> bool:
     for _char in s:
         if '\u4e00' <= _char <= '\u9fa5':
             return True
     return False
 
-
 def load_csv_files() -> List[CsvFile]:
+    """
+    根据 para_tranz_map.json 设置，批量读取原文、译文csv文件
+    :return: CsvFile文件对象列表
+
+    para_tranz_map.json 的格式如下：
+    [
+        {
+            "path": "csv文件路径，使用'/'作分隔符.csv",
+            "id_column_name": "csv中作为id的列名",
+            "text_column_names": [
+              "需要翻译列的列名1",
+              "需要翻译列的列名2"
+            ]
+        },
+        {
+            "path": "csv文件路径，使用'/'作分隔符.csv",
+            "id_column_name": ["作为id的列名1", "作为id的列名2"],
+            "text_column_names": [
+              "需要翻译列的列名1",
+              "需要翻译列的列名2"
+            ]
+        }
+    ]
+    """
     logger.info('开始读取游戏原文与译文数据')
     with open(PROJECT_DIRECTORY / 'para_tranz_map.json') as f:
         d = json.load(f)
@@ -232,6 +277,7 @@ def paratranz_to_csv():
 
 
 # From processWithWiredChars.py
+# 由于游戏原文文件中可能存在以Windows-1252格式编码的字符（如前后双引号等），所以需要进行转换
 def replace_weird_chars(s: str) -> str:
     return s.replace('\udc94', '""').replace('\udc93', '""').replace('\udc92', "'").replace(
         '\udc85', '...')
